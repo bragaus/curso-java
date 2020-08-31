f8a2fd9c0f8e77816a5dc8a234fc494f9c857c8c
bragaus <mannoplay@gmail.com>
2020-08-31 06:47:08 -0300

    declarando atributos

--- ooParte2/src/ooParte2/Main.java
+++ ooParte2/src/ooParte2/Main.java
@@ -2,36 +2,41 @@ package ooParte2;
 
 public class Main {
 	
-	private static int inteiro() {
-		return 10;
-	}
+//	private static int inteiro() {
+//		return 10;
+//	}
 	
-	private static void Arrays(int[] n1, String[] nome) {
-		System.out.println(n1[0]);
-		System.out.println(nome[0]);
-	}
+//	private static void Arrays(int[] n1, String[] nome) {
+//		System.out.println(n1[0]);
+//		System.out.println(nome[0]);
+//	}
+	
+	private String nome;
+	public static final int VIDA_MAXIMA = 100;
 	
 	// Método Construtor
 	public Main() {
-		System.out.println("Classe criada!");
+		nome = "Matheus";
+		System.out.println("Classe criada! " + nome);
+		System.out.println("Vida maxima " + VIDA_MAXIMA);
 	}	
 	
 	public static void main(String[] args) {
 		
 		new Main();
 		
-		Player player = new Player();
-		
-		player.iniciarPlayer();
-		
-		System.out.println(inteiro());
-		
-		int[] n1 = new int[10];
-		n1[0] = 10;
-		String[] nomes = new String[2];
-		nomes[0] = "João";
-		
-		Arrays(n1, nomes);
+//		Player player = new Player();
+//		
+//		player.iniciarPlayer();
+//		
+//		System.out.println(inteiro());
+//		
+//		int[] n1 = new int[10];
+//		n1[0] = 10;
+//		String[] nomes = new String[2];
+//		nomes[0] = "João";
+//		
+//		Arrays(n1, nomes);
 	}
 	
 
